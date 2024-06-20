52ce96bc769f7eea0ab2f54763ecef02a78bbe9e
Gihanmadhusankha <gmjayaushantha2007@gmail.com>
2024-06-20 16:42:52 +0530

    c1

---
+++ todolist/src/Create.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import axios from 'axois'
+
+const Create = () => {
+    const [task,setTask]=useState()
+    const handleAdd =() =>{
+       axios.post('http://localhost:5000/add',{task:task})
+       .then(result=>{
+         location.reload()
+       })
+       .catch(err=>console.log(err))
+
+    }
+  return (
+    <div>
+        <input type="text" placeholder='Enter Task' onChange={(e)=>setTask(e.target.value)} />
+        <button type="button" onClick={handleAdd}>Submit</button>
+
+
+
+    </div>
+  )
+}
+
+export default Create
